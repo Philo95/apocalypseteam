20bf6344cf1e23fbd74f046bca0e88a70ce62db4
Philo95 <113151552+Philo95@users.noreply.github.com>
2023-07-23 22:58:45 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+console.log("JavaScript Portion of the Test");
+
+
+
+
+const myImmutableArray = ["Jared", "Francis", "Amrit", "Sarah", "Gurleen", "Johanna", "Ury"];
+
+
+
+const myArray = ["Jared", "Francis", "Amrit"];
+
+
+myArray.shift();
+
+
+
+myArray.shift(1);
+
+
+
+;
+const image = document.querySelector('button');
+image.addEventListener("click", function(){ 
+    console.log("Call now at 1-555-555-5555");
+    alert("Call now at 1-555-555-5555")
+});
